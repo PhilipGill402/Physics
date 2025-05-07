194ff2b454f39520b9a8a2b67f82152f493f2af8
FurryBasket9978 <pgillbama@icloud.com>
2025-05-07 17:48:49 -0500

    finished bouncy ball demo

--- Ball.py
+++ Ball.py
@@ -1,41 +1,39 @@
 from Constants import *
+import numpy as np
 import pygame
 
 class Ball:
-    def __init__(self, x:int, y:int, mass:int):
-        self.x = x
-        self.y = y
+    def __init__(self, x:float, y:float, mass:float):
         self.mass = mass
-        self.vx = 0
-        self.vy = 0
-        self.ax = 0
-        self.ay = 0
-
-    def setVelocity(self, vx:int=0, vy:int=0):
-        self.vx = vx
-        self.vy = vy
+        self.position = np.array([x, y])
+        self.velocity = np.array([0.0,0.0])
+        self.acceleration = np.array([0.0,0.0])
 
     def addForce(self, forceX:int=0, forceY:int=0):
-        self.ax += (forceX/self.mass)
-        self.ay += (forceY/self.mass)
-    
+        self.acceleration[0] += (forceX/self.mass)
+        self.acceleration[1] += (forceY/self.mass)
+
     def collidesWithBoundaries(self) -> bool:
-        if self.y > HEIGHT - 50 or self.x > WIDTH or self.x < 0:
+        if self.position[1] > HEIGHT - 50 or self.position[1] < 0 or self.position[0] > WIDTH or self.position[0] < 0:
             return True
         return False
     
     def drawBall(self, surface, color:tuple):
-        pygame.draw.circle(surface, color, (self.x, self.y), self.mass)
+        pygame.draw.circle(surface, color, (self.position[0], self.position[1]), self.mass)
     
     def updateVelocty(self):
-        self.vx += self.ax
-        self.vy += self.ay
+        self.velocity += self.acceleration
     
     def updatePosition(self):
-        self.x += self.vx
-        self.y += self.vy
-    
+        self.position += self.velocity 
+
+    def collide(self):
+        if self.collidesWithBoundaries():
+            self.position[1] = HEIGHT - 50 - self.mass 
+            self.velocity *= -1
+             
     def update(self, surface, color:tuple):
         self.updateVelocty()
         self.updatePosition()
+        self.collide()
         self.drawBall(surface, color)
--- BouncingBall.py
+++ BouncingBall.py
@@ -1,23 +1,32 @@
 import pygame
+import time
+import numpy as np
 from Constants import *
 from Ball import *
 
+# REMEMBER: right is positive and left is negative
+# REMEMBER: up is negative and down is positive
 pygame.init()
 surface = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Path Finding")
+pygame.display.set_caption("Bouncy Ball")
 running = True
-bouncyBall = Ball(100,100,10)
+bouncyBall = Ball(100.0,100.0,10.0)
+gravity = bouncyBall.mass * 5
 
 while running:
     surface.fill(BLACK)
     pygame.draw.rect(surface, WHITE, pygame.Rect(0, HEIGHT-50, WIDTH, 50))
-    gravity = bouncyBall.mass * -9.8
-    bouncyBall.addForce(0,gravity)
-    bouncyBall.update(surface, WHITE)
+    
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
             running = False
-
+        if ev.type == pygame.KEYDOWN:
+            if ev.key == pygame.K_SPACE:
+                bouncyBall.addForce(0,gravity)
+    
+    bouncyBall.update(surface, WHITE)
+    print(bouncyBall.acceleration)
     pygame.display.update()
+    time.sleep(0.025)
 
 pygame.quit()
